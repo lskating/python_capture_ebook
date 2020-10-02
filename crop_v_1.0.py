1c75e2a7dae3cd40e2d40f305e8681c072524827
simon <2441873702@qq.com>
2020-10-02 21:42:41 +0800

    1. fix the save file error 2. add input pages number distinguish
    Signed-off-by: simon <2441873702@qq.com>

--- crop_v_1.0.py
+++ crop_v_1.0.py
@@ -18,49 +18,23 @@ import win32gui
 
 import ctypes
 
-####################################################
-pages = int(input("pages = "))
-####################################################
-# screen_x = win32api.GetSystemMetrics(win32con.SM_CXSCREEN)
-# screen_y = win32api.GetSystemMetrics(win32con.SM_CYSCREEN)
-# print(screen_x,screen_y)
-
-hDC = win32gui.GetDC(0)
-w = win32print.GetDeviceCaps(hDC, win32con.DESKTOPHORZRES)
-h = win32print.GetDeviceCaps(hDC, win32con.DESKTOPVERTRES)
-# print(w,h)
-
-for num in range(2,0,-1):   # 3 seconds
+def timer():
+    for num in range(2,0,-1):   # 3 seconds
+        os.system('cls')
+        print("--------------------------------------------")
+        print("    please switch to the capture screen!")
+        print("     this will start in {} second(s).".format(num))
+        print("--------------------------------------------")
+        if num == 1:
+            ctypes.windll.user32.ShowWindow(ctypes.windll.kernel32.GetConsoleWindow(),6)        # minimis
+            # refer to: https://www.cnblogs.com/candyzkn/p/4428590.html
+        else:
+            pass
+        time.sleep(1)
     os.system('cls')
     print("--------------------------------------------")
-    print("    please switch to the capture screen!")
-    print("     this will start in {} second(s).".format(num))
+    print("                  doing                     ")
     print("--------------------------------------------")
-    if num == 1:
-        ctypes.windll.user32.ShowWindow(ctypes.windll.kernel32.GetConsoleWindow(),6)        # minimis
-        # refer to: https://www.cnblogs.com/candyzkn/p/4428590.html
-    else:
-        pass
-    time.sleep(1)
-os.system('cls')
-print("--------------------------------------------")
-print("                  doing                     ")
-print("--------------------------------------------")
-
-date_now = datetime.datetime.now().strftime('%Y%m%d')
-File_Path = os.getcwd()	+ "\\"	#获取到当前文件的目录
-img_temp_path = File_Path + "temp\\"
-
-#采取新建文件夹的方式避免删除已有temp文件夹中的文件
-for i in range(1,100):
-    if os.path.exists(img_temp_path):
-        img_temp_path = File_Path + "temp_" + str(str(i).zfill(2)) + "\\"
-    elif not os.path.exists(img_temp_path):
-        os.makedirs(img_temp_path)                         
-        break
-
-height = 0
-weight = 0
 
 def getBookSize(picture_i):
     image = cv2.imread(picture_i)
@@ -134,7 +108,7 @@ def getBookSize(picture_i):
     white_bottom = max(white_edges_y)
     # print("white_top,white_bottom = ",white_top,white_bottom)
     pic_size = (white_left, white_top, white_right, white_bottom)
-    print(pic_size)
+    # print(pic_size)
     return pic_size
 
 
@@ -184,66 +158,119 @@ def PageDown():
     time.sleep(1.2)
 
 
-x = range(1,pages + 1)
-# default_size = (0,0,weight,height)
-default_size = (0,0,w,h)
-# print(default_size)
-size_all = default_size
+try:
+    ####################################################
+    pages_str = input("please input the number of total pages (with 0~9) = ")
+    while not pages_str.isdigit() or not int(pages_str):
+        pages_str = input("input erro! please input the number (with 0~9) = ")
+    pages = int(pages_str)
+    ####################################################
+    # screen_x = win32api.GetSystemMetrics(win32con.SM_CXSCREEN)
+    # screen_y = win32api.GetSystemMetrics(win32con.SM_CYSCREEN)
+    # print(screen_x,screen_y)
 
-for num in range(1,6):
-    if num <= 3:
-        PageDown()
-    elif num == 4:
-        picture_temp = img_temp_path + str(date_now) + "_size_crop.png"
-        picGrab(picture_temp,default_size)
-        size_all = (x0,y0,x1,y1) =  getBookSize(picture_temp)
-        size_left = (x0,y0,int((x1+x0)/2),y1)
-        size_right = (int((x1+x0)/2),y0,x1,y1)
-        size_center = (int((3*x0+x1)/4),y0,int((3*x1+x0)/4),y1)
-        os.remove(picture_temp)
-        PageUp()
-        PageUp()
-        PageUp()
+    hDC = win32gui.GetDC(0)
+    w = win32print.GetDeviceCaps(hDC, win32con.DESKTOPHORZRES)
+    h = win32print.GetDeviceCaps(hDC, win32con.DESKTOPVERTRES)
+    # print(w,h)
+
+    timer()
+    date_now = datetime.datetime.now().strftime('%Y%m%d')
+    File_Path = os.getcwd() + "\\"  #获取到当前文件的目录
+    img_temp_path = File_Path + "temp\\"
+
+    #采取新建文件夹的方式避免删除已有temp文件夹中的文件
+    for i in range(1,100):
+        if os.path.exists(img_temp_path):
+            img_temp_path = File_Path + "temp_" + str(str(i).zfill(2)) + "\\"
+        elif not os.path.exists(img_temp_path):
+            os.makedirs(img_temp_path)                         
+            break
+    height = 0
+    weight = 0
+
+    x = range(1,pages + 1)
+    # default_size = (0,0,weight,height)
+    default_size = (0,0,w,h)
+    # print(default_size)
+    size_all = default_size
+
+    for num in range(1,6):
+        if num <= 3:
+            PageDown()
+        elif num == 4:
+            picture_temp = img_temp_path + str(date_now) + "_size_crop.png"
+            picGrab(picture_temp,default_size)
+            size_all = (x0,y0,x1,y1) =  getBookSize(picture_temp)
+            size_left = (x0,y0,int((x1+x0)/2),y1)
+            size_right = (int((x1+x0)/2),y0,x1,y1)
+            size_center = (int((3*x0+x1)/4),y0,int((3*x1+x0)/4),y1)
+            os.remove(picture_temp)
+            PageUp()
+            PageUp()
+            PageUp()
+        else:
+            for i in x:
+                picture_i = img_temp_path + str(date_now) + "_" + str(str(i).zfill(4)) + ".png"
+                if i == 1:
+                    picGrab(picture_i,size_center)
+                    PageDown()
+                elif  i == pages:
+                    picGrab(picture_i,size_center)
+                elif i % 2 == 0:
+                    picGrab(picture_i,size_left)
+                else:
+                    picGrab(picture_i,size_right)
+                    PageDown()
+    os.system('cls')
+    ctypes.windll.user32.ShowWindow(ctypes.windll.kernel32.GetConsoleWindow(),9)        # 3
+    # 20201002 changed the name of pdfFile
+    auto_pdfFile = File_Path + str(date_now) + "_00.pdf"
+    for i in range(1,100):
+        if os.path.isfile(auto_pdfFile):
+            auto_pdfFile = File_Path + str(date_now) + "_" + str(str(i).zfill(2)) + ".pdf"
+        else:
+            break
+    check_box = ['y','Y','n','N']
+    rename_check = str(input("Do you want to rename the file? [y]/n "))[0]
+    while rename_check not in check_box:
+        rename_check = str(input("please input [y]/n "))
+
+    # bugreport: when input chinese book name, there would be some error leading to save file failed. / 2nd.Oct.2020
+    # add error char
+
+    error_char = ["\\","/","：","*","？","<",">","|","\"","“","”","'","‘","’","?",]
+    if rename_check == 'y' or rename_check == 'Y':
+        name_temp = str(input("please input the name of this book: "))
+        flag = 0
+        while not flag:
+            while not len(name_temp):
+                name_temp = str(input("please input the name of this book (not blank): "))
+            for char in error_char:
+                # print(flag)
+                flag = flag + 1         # 8
+                if char in name_temp:
+                    print("文件名不能包含下列字符：")
+                    print(error_char)
+                    name_temp = str(input("please input the name of this book: "))
+                    flag = 0
+                    break
+        file_name = File_Path + name_temp + ".pdf"
     else:
-        for i in x:
-            picture_i = img_temp_path + str(date_now) + "_" + str(str(i).zfill(4)) + ".png"
-            if i == 1:
-                picGrab(picture_i,size_center)
-                PageDown()
-            elif  i == pages:
-                picGrab(picture_i,size_center)
-            elif i % 2 == 0:
-                picGrab(picture_i,size_left)
-            else:
-                picGrab(picture_i,size_right)
-                PageDown()
+        file_name = auto_pdfFile
 
+    os.system('cls')
+    combine2Pdf(img_temp_path, file_name)
+    print("-------------------------------------------------------------------")
+    print("file has been saved as: \n" + file_name)
+    print("-------------------------------------------------------------------")
+    del_file(img_temp_path)
+    os.rmdir(img_temp_path)
 
-ctypes.windll.user32.ShowWindow(ctypes.windll.kernel32.GetConsoleWindow(),9)        # 3
-# 20201002 changed the name of pdfFile
-auto_pdfFile = File_Path + str(date_now) + "_00.pdf"
-for i in range(1,100):
-    if os.path.isfile(auto_pdfFile):
-        auto_pdfFile = File_Path + str(date_now) + "_" + str(str(i).zfill(2)) + ".pdf"
-    else:
-        break
-check_box = ['y','Y','n','N']
-rename_check = str(input("Do you want to rename the file? [y]/n "))
-while rename_check not in check_box:
-    rename_check = str(input("please input [y]/n "))
-
-if rename_check == 'y' or rename_check == 'Y':
-    name_temp = str(input("please input the name of this book: "))
-    while not len(name_temp):
-        name_temp = str(input("please input the name of this book (not blank): "))
-    file_name = File_Path + name_temp + ".pdf"
-else:
-    file_name = auto_pdfFile
-
-os.system('cls')
-combine2Pdf(img_temp_path, file_name)
-print("-------------------------------------------------------------------")
-print("file has been saved as " + file_name)
-print("-------------------------------------------------------------------")
-del_file(img_temp_path)
-os.rmdir(img_temp_path)
+except:
+    os.system('cls')
+    ctypes.windll.user32.ShowWindow(ctypes.windll.kernel32.GetConsoleWindow(),9)
+    # https://www.cnblogs.com/daofaziran/p/9015284.html
+    print("-------------------------------------------------------------------")
+    print("\033[1;31m                           !!!except!!!                            \033[0m")
+    print("-------------------------------------------------------------------")
